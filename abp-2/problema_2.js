86949c0e2b2ac154bacdb3b82ff22747c19142fc
RochCX <rodrigo.pequeno.24@gmail.com>
2023-03-14 17:41:07 -0300

    pequeños detalles

---
+++ abp-2/problema_2.js
@@ -0,0 +1,7 @@
+
+function plusFamilia() {
+    document.querySelector(".familia-container").innerHTML+=`
+        <div class="familiaADD">
+            <input type="text" name="familia" id="integrante">
+    `
+}
